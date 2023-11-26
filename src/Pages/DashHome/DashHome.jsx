064a06266fa3c89bf54387b22632e5497e96c316
eflexTech <eflexmedia1@gmail.com>
2023-11-26 12:19:09 -0800

    All Good

--- src/Pages/DashHome/DashHome.jsx
+++ src/Pages/DashHome/DashHome.jsx
@@ -10,13 +10,42 @@ import "./DashHome.css";
 import {FaSackDollar, FaDatabase} from "react-icons/fa6";
 
 import {useSelector} from 'react-redux'
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 
 const DashHome = () => {
-
+    const [exchangeRate, setExchangeRate] = useState(null);
     const userData = useSelector((state) => state.swift.user)
     console.log(userData);
 
+    useEffect(() => {
+        // Fetch the current exchange rate from an API (replace with a reliable API)
+        axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+          .then(response => {
+            const rate = response.data.bpi.USD.rate.replace(',', ''); // assuming USD rate
+            setExchangeRate(parseFloat(rate));
+          })
+          .catch(error => {
+            console.error('Error fetching exchange rate:', error);
+          });
+      }, []); // Empty dependency array ensures useEffect runs only once on component mount
+
+      const bitcoinValue = userData.accountBalance / exchangeRate;
+      const bitcoinValue2 = userData.totalProfit / exchangeRate;
+      const bitcoinValue3 = userData.bonus / exchangeRate;
+      const bitcoinValue4 = userData.ref / exchangeRate;
+      const bitcoinValue5 = userData.totalDeposit / exchangeRate;
+      const bitcoinValue6 = userData.totalWithdrawal / exchangeRate;
+      const bitcoinValue7 = userData.totalInvestment / exchangeRate;
+      const roundedNumber = parseFloat(bitcoinValue.toFixed(8));
+      const roundedNumber2 = parseFloat(bitcoinValue2.toFixed(8));
+      const roundedNumber3 = parseFloat(bitcoinValue3.toFixed(8));
+      const roundedNumber4 = parseFloat(bitcoinValue4.toFixed(8));
+      const roundedNumber5 = parseFloat(bitcoinValue5.toFixed(8));
+      const roundedNumber6 = parseFloat(bitcoinValue6.toFixed(8));
+      const roundedNumber7 = parseFloat(bitcoinValue7.toFixed(8));
+      console.log("this is it",roundedNumber);
 
     return (
         <>
@@ -40,6 +69,7 @@ const DashHome = () => {
                                     <div className="DashHomeMainContentAccSummaryRow1BoxL">
                                         <h4>Account Balance</h4>
                                         <h3>$ &nbsp;{userData.accountBalance}.00</h3>
+                                        <span style={{fontWeight:"700"}}>{roundedNumber}BTC</span>
                                     </div>
                                     <div className="DashHomeMainContentAccSummaryRow1BoxR">
                                         <span>
@@ -51,6 +81,7 @@ const DashHome = () => {
                                     <div className="DashHomeMainContentAccSummaryRow1BoxL">
                                         <h4>Total Profit</h4>
                                         <h3>$ &nbsp;{userData.totalProfit}.00</h3>
+                                        <span style={{fontWeight:"700"}}>{roundedNumber2}BTC</span>
                                     </div>
                                     <div className="DashHomeMainContentAccSummaryRow1BoxR">
                                         <span>
@@ -62,6 +93,7 @@ const DashHome = () => {
                                     <div className="DashHomeMainContentAccSummaryRow1BoxL">
                                         <h4>Bonus</h4>
                                         <h3>$ &nbsp;{userData.bonus}.00</h3>
+                                        <span style={{fontWeight:"700"}}>{roundedNumber3}BTC</span>
                                     </div>
                                     <div className="DashHomeMainContentAccSummaryRow1BoxR">
                                         <span>
@@ -74,6 +106,7 @@ const DashHome = () => {
                                     <div className="DashHomeMainContentAccSummaryRow2BoxL">
                                         <h4>Referral Bonus</h4>
                                         <h3>$ &nbsp;{userData.ref}.00</h3>
+                                        <span style={{fontWeight:"700"}}>{roundedNumber4}BTC</span>
                                     </div>
                                     <div className="DashHomeMainContentAccSummaryRow2BoxR">
                                         <span>
@@ -85,6 +118,7 @@ const DashHome = () => {
                                     <div className="DashHomeMainContentAccSummaryRow2BoxL">
                                         <h4>Total Deposits</h4>
                                         <h3>$ &nbsp;{userData.totalDeposit}.00</h3>
+                                        <span style={{fontWeight:"700"}}>{roundedNumber5}BTC</span>
                                     </div>
                                     <div className="DashHomeMainContentAccSummaryRow2BoxR">
                                         <span>
@@ -96,6 +130,7 @@ const DashHome = () => {
                                     <div className="DashHomeMainContentAccSummaryRow2BoxL">
                                         <h4>Total Widthdrawal</h4>
                                         <h3>$ &nbsp;{userData.totalWithdrawal}.00</h3>
+                                        <span style={{fontWeight:"700"}}>{roundedNumber6}BTC</span>
                                     </div>
                                     <div className="DashHomeMainContentAccSummaryRow2BoxR">
                                         <span>
@@ -107,6 +142,7 @@ const DashHome = () => {
                                     <div className="DashHomeMainContentAccSummaryRow2BoxL">
                                         <h4>Total Investment</h4>
                                         <h3>$ &nbsp;{userData.totalInvestment}.00</h3>
+                                        <span style={{fontWeight:"700"}}>{roundedNumber7}BTC</span>
                                     </div>
                                     <div className="DashHomeMainContentAccSummaryRow2BoxR">
                                         <span>
