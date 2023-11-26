0cb7bcff44ea54a649b6741aec636debf45a063f
RaphealJames07 <123210977+RaphealJames07@users.noreply.github.com>
2023-11-26 18:08:44 +0100

    did

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,7 @@
 
 import "./App.css";
-import ScrollToTop from "./Components/ScrollToTop";
 import { HashRouter, Route, Routes } from "react-router-dom";
 import Dashboard from "./Components/Dashboard/Dashboard";
-// import DashHome from "./Pages/DashHome/DashHome";
-// import Deposit from "./Pages/Deposit/Deposit";
-// import Withdrawal from "./Pages/Withdrawal/Withdrawal";
-// import ProfitHistory from "./Pages/ProfitHistory/ProfitHistory";
-// import Transactions from "./Pages/Transactions/Transactions";
-// import Transfer from "./Pages/TransferFunds/TransferFunds";
-// import Profile from "./Pages/Profile/Profile";
-// import TradingPlans from "./Pages/TradingPlans/TradingPlans";
-// import MyPlans from "./Pages/MyPlans/MyPlans";
-// import Referrals from "./Pages/Referrals/Referrals";
-// import WithdrawFunds from "./Pages/Withdrawal/WithdrawFunds";
-// import Payment from "./Pages/Deposit/Payment";
-// import HomeRoute from "./routes/HomeRoute";
-// // import HomeRoute from "./routes/HomeRoute";
-// import { HashRouter, Route, Routes } from "react-router-dom";
-
-
-
-
-
-// function App() {  
-  
-//   return (
-//     <>
-//          <HashRouter>
-//           <Routes>
-//             <Route path="/" element={<Dashboard />} />
-//           </Routes>
-//         </HashRouter>
-//     </>
-//   )
-// }
-
-// export default App
 
 
 const App = () => {
@@ -44,7 +9,7 @@ const App = () => {
     <>
         <HashRouter>
           <Routes>
-            <Route path="/Dashboard" element={<Dashboard/>} />
+            <Route path="/" element={<Dashboard/>} />
           </Routes>
         </HashRouter>
     </>
